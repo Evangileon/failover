01f249df340a8512acdcd20f4527c54ffe25c417
eageon <yujun.eageon@gmail.com>
2014-09-04 00:12:46 -0500

    heartbeat not work

--- heartbeat.cpp
+++ heartbeat.cpp
@@ -17,9 +17,9 @@
 #include "heartbeat.h"
 
 heartbeat::heartbeat() {
-    needSend = 1;
-    status_recv_thread_id = -1;
-    status_send_thread_id = -1;
+	needSend = 1;
+	status_recv_thread_id = -1;
+	status_send_thread_id = -1;
 }
 
 heartbeat::~heartbeat() {
@@ -27,190 +27,189 @@ heartbeat::~heartbeat() {
 }
 
 int heartbeat::heartbeat_receive_loop(int rfd) {
-    fd_set rfds;
-    int ret = -1;
-    char buffer[16];
-    unsigned timeout = config::instance().get_heartbeat_receive_loop_timeout();
+	fd_set rfds;
+	int ret = -1;
+	char buffer[16];
+	unsigned timeout = config::instance().get_heartbeat_receive_loop_timeout();
 
-    while (1) {
-        ret = select_with_timeout(rfd, &rfds, timeout);
-        if (ret <= 0) {
-            break;
-        }
-        if (!FD_ISSET(rfd, &rfds)) {
-            break;
-        }
+	while (1) {
+		ret = select_with_timeout(rfd, &rfds, timeout);
+		if (ret <= 0) {
+			break;
+		}
+		if (!FD_ISSET(rfd, &rfds)) {
+			break;
+		}
 
-        ret = recv(rfd, buffer, 16, 0);
-        if (ret != 16) {
-            break;
-        }
-        notify_observers(THE_OTHER_IS_ALIVE);
-        std::cout << "heartbeat recv succeed" << std::endl;
-        sleep(1);
-    }
+		ret = recv(rfd, buffer, 16, 0);
+		if (ret != 16) {
+			break;
+		}
+		notify_observers(THE_OTHER_IS_ALIVE);
+		std::cout << "heartbeat recv succeed" << std::endl;
+		sleep(1);
+	}
 
-    return ret;
+	return ret;
 }
 
 int heartbeat::heartbeat_send_loop(int wfd) {
-    fd_set wfds;
-    int ret = -1;
-    char buffer[16];
-    unsigned interval = config::instance().get_heartbeat_send_interval();
-    unsigned timeout = config::instance().get_heartbeat_send_loop_timeout();
+	fd_set wfds;
+	int ret = -1;
+	char buffer[16];
+	unsigned interval = config::instance().get_heartbeat_send_interval();
+	unsigned timeout = config::instance().get_heartbeat_send_loop_timeout();
 
-    while (1) {
-        sleep(interval);
+	while (1) {
+		sleep(interval);
 
-        ret = select_write_with_timeout(wfd, &wfds, timeout);
-        if (ret <= 0) {
-            break;
-        }
-        if (!FD_ISSET(wfd, &wfds)) {
-            break;
-        }
+		ret = select_write_with_timeout(wfd, &wfds, timeout);
+		if (ret <= 0) {
+			break;
+		}
+		if (!FD_ISSET(wfd, &wfds)) {
+			break;
+		}
 
-        ret = send(wfd, buffer, 16, MSG_NOSIGNAL);
-        //DEBUG("errno = %d\n", errno);
-        if (ret != 16) {
-            break;
-        }
-        std::cout << "heartbeat send succeed" << std::endl;
-    }
+		ret = send(wfd, buffer, 16, MSG_NOSIGNAL);
+		//DEBUG("errno = %d\n", errno);
+		if (ret != 16) {
+			break;
+		}
+		std::cout << "heartbeat send succeed" << std::endl;
+	}
 
-    return ret;
+	return ret;
 }
 
 void heartbeat::heartbeat_receive() {
-    heartbeat_recv_thread_id = std::this_thread::get_id();
+	heartbeat_recv_thread_id = std::this_thread::get_id();
 
-    int sockfd;
-    int ret;
-    int status = 0;
-    unsigned timeout = config::instance().get_heartbeat_receive_timeout();
-    fd_set rfds;
+	int sockfd;
+	int ret;
+	int status = 0;
+	unsigned timeout = config::instance().get_heartbeat_receive_timeout();
+	fd_set rfds;
 
-    struct sockaddr_in cli_addr;
-    socklen_t cli_len;
-    cli_len = sizeof(cli_addr);
+	struct sockaddr_in cli_addr;
+	socklen_t cli_len;
+	cli_len = sizeof(cli_addr);
 
-    sockfd = get_tcp_connection_ready(
-                 config::instance().get_ip_heartbeat_sender().c_str(),
-                 config::instance().get_port_heartbeat_receiver(), MAX_CONN_COUNT);
-    if ((sockfd) < 0) {
-        perror("ERROR opening socket");
-        ERROR("%d\n", __LINE__);
-    }
+	while (1) {
+		sockfd = get_tcp_connection_ready(
+				config::instance().get_ip_heartbeat_sender().c_str(),
+				config::instance().get_port_heartbeat_receiver(),
+				MAX_CONN_COUNT);
+		if ((sockfd) < 0) {
+			perror("ERROR opening socket");
+			ERROR("%d\n", __LINE__);
+		}
 
-    while (1) {
-        //sockfd = get_any_tcp_connection_ready_socket(sockfd, config::instance().get_port_heartbeat_recv(), MAX_CONN_COUNT);
-        if (sockfd < 0) {
-            ERROR("%s, %d\n", __FILE__, __LINE__);
-        }
-
-        if (config::instance().is_heartbeat_direct_link()) {
+		if (config::instance().is_heartbeat_direct_link()) {
 			enable_direct_link(sockfd);
 		}
 
-        ret = select_with_timeout(sockfd, &rfds, timeout);
-        if (ret < 0) {
-            perror("after select");
-            ERROR("%s, %d\n", __FILE__, __LINE__);
-        } else if (ret == 0) {
-            // timeout
-            // the other one is dead
-            status = -1;
-            needSend = 0;
-            std::cout << "The other is dead" << std::endl;
-            notify_observers(THE_OTHER_IS_DEAD);
-        } else {
-            // nothing to do
-            std::cout << "go to heartbeat receive loop" << std::endl;
-            int cfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
-            if (cfd < 0) {
-                perror("It shouldn't be error");
-            }
-            needSend = 1;
-            notify_observers(THE_OTHER_IS_ALIVE);
-            heartbeat_receive_loop(cfd);
-            close(cfd);
-        }
-        if (status < 0) {
-        }
-    }
+		ret = select_with_timeout(sockfd, &rfds, timeout);
+		if (ret < 0) {
+			perror("after select");
+			ERROR("%s, %d\n", __FILE__, __LINE__);
+		} else if (ret == 0) {
+			// timeout
+			// the other one is dead
+			status = -1;
+			needSend = 0;
+			std::cout << "The other is dead" << std::endl;
+			sleep(1);
+			notify_observers(THE_OTHER_IS_DEAD);
+		} else {
+			// nothing to do
+			std::cout << "go to heartbeat receive loop" << std::endl;
+			int cfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
+			if (cfd < 0) {
+				perror("It shouldn't be error");
+			}
+			needSend = 1;
+			notify_observers(THE_OTHER_IS_ALIVE);
+			heartbeat_receive_loop(cfd);
+			close(cfd);
+		}
+		close(sockfd);
+	}
 
-    close(sockfd);
+	close(sockfd);
 }
 
 void heartbeat::heartbeat_send() {
-    heartbeat_send_thread_id = std::this_thread::get_id();
+	heartbeat_send_thread_id = std::this_thread::get_id();
 
-    int sockfd;
+	int sockfd;
 
-    char receiver_addr[20];
-    std::string receiver_addr_s = config::instance().get_ip_heartbeat_receiver();
-    int receiver_port = config::instance().get_port_heartbeat_receiver();
-    receiver_addr_s.copy(receiver_addr, receiver_addr_s.length());
-    unsigned timeout = config::instance().get_connect_nonblock_timeout();
+	char receiver_addr[20];
+	std::string receiver_addr_s =
+			config::instance().get_ip_heartbeat_receiver();
+	int receiver_port = config::instance().get_port_heartbeat_receiver();
+	receiver_addr_s.copy(receiver_addr, receiver_addr_s.length());
+	unsigned timeout = config::instance().get_connect_nonblock_timeout();
 
-    std::cout << "heartbeat receiver addr: " << receiver_addr << ":"
-              << receiver_port << std::endl;
+	std::cout << "heartbeat receiver addr: " << receiver_addr << ":"
+			<< receiver_port << std::endl;
 
-    //char sender_addr[20];
-    //std::string sender_addr_s = config::instance().get_ip_heartbeat_sender();
-    int sender_port = config::instance().get_port_heartbeat_sender();
-    //sender_addr_s.copy(sender_addr, sender_addr_s.length());
+	//char sender_addr[20];
+	//std::string sender_addr_s = config::instance().get_ip_heartbeat_sender();
+	int sender_port = config::instance().get_port_heartbeat_sender();
+	//sender_addr_s.copy(sender_addr, sender_addr_s.length());
 
-    std::cout << "heartbeat sender bind to addr: " << config::instance().get_ip_heartbeat_sender() << ":"
-              << sender_port << std::endl;
+	std::cout << "heartbeat sender bind to addr: "
+			<< config::instance().get_ip_heartbeat_sender() << ":"
+			<< sender_port << std::endl;
 
-    while (1) {
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) {
-            ERROR("%d\n", __LINE__);
-        }
+	while (1) {
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0) {
+			ERROR("%d\n", __LINE__);
+		}
 
-        if(config::instance().is_heartbeat_direct_link()) {
-        	enable_direct_link(sockfd);
-        }
+		if (config::instance().is_heartbeat_direct_link()) {
+			enable_direct_link(sockfd);
+		}
 
-        if(sender_bind(NULL, sender_port, sockfd) < 0) {
-        	std::cout << "Can not bind" << std::endl;
-        }
+		if (sender_bind(NULL, sender_port, sockfd) < 0) {
+			std::cout << "Can not bind" << std::endl;
+		}
 
-        int ret = connect_nonblock(receiver_addr, receiver_port, sockfd,
-                                   timeout);
-        if (ret < 0) {
-            if (errno == ECONNREFUSED) {
-                std::cout << "connection refused" << std::endl;
-                sleep(1);
-                continue;
-            }
+		int ret = connect_nonblock(receiver_addr, receiver_port, sockfd,
+				timeout);
+		if (ret < 0) {
+			if (errno == ECONNREFUSED) {
+				std::cout << "connection refused" << std::endl;
+				close(sockfd);
+				sleep(1);
+				continue;
+			}
 
-            perror("heartbeat connect after nonblock");
-            printf("Requested address: %s:%d\n", receiver_addr, receiver_port);
-            CUR_INFO();
-            //ERROR("%s:%d\n", __FILE__, __LINE__);
-        }
+			perror("heartbeat connect after nonblock");
+			printf("Requested address: %s:%d\n", receiver_addr, receiver_port);
+			CUR_INFO();
+		}
 
-        if (errno == ETIMEDOUT) {
-            // time out
-            continue;
-        }
+		if (errno == ETIMEDOUT) {
+			// time out
+			close(sockfd);
+			continue;
+		}
 
-        std::cout << "go to send loop" << std::endl;
-        heartbeat_send_loop(sockfd);
-        std::cout << "cease sending" << std::endl;
-        close(sockfd);
-    }
+		std::cout << "go to send loop" << std::endl;
+		heartbeat_send_loop(sockfd);
+		std::cout << "cease sending" << std::endl;
+		close(sockfd);
+	}
 }
 
 void heartbeat::notify_observers(int flag) {
-    for (std::vector<std::shared_ptr<observer> >::iterator iter = views.begin();
-            iter != views.end(); ++iter) {
-        (*iter)->update(flag);
-    }
+	for (std::vector<std::shared_ptr<observer> >::iterator iter = views.begin();
+			iter != views.end(); ++iter) {
+		(*iter)->update(flag);
+	}
 }
 
 void heartbeat::start_heartbeat_send() {
@@ -222,13 +221,13 @@ void heartbeat::start_heartbeat_recv() {
 }
 
 std::shared_ptr<heartbeat> init_heartbeat() {
-    heartbeat *hb = new heartbeat();
-    std::thread hbRecv(&heartbeat::heartbeat_send, hb);
-    hbRecv.detach();
-    sleep(4);
-    std::thread hbSend(&heartbeat::heartbeat_receive, hb);
-    hbSend.detach();
+	heartbeat *hb = new heartbeat();
+	std::thread hbRecv(&heartbeat::heartbeat_send, hb);
+	hbRecv.detach();
+	sleep(4);
+	std::thread hbSend(&heartbeat::heartbeat_receive, hb);
+	hbSend.detach();
 
-    std::shared_ptr<heartbeat> hb_ptr(hb);
-    return hb_ptr;
+	std::shared_ptr<heartbeat> hb_ptr(hb);
+	return hb_ptr;
 }
